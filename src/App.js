dfb6d6d7c0764dbd1b535834d8abfe2aa20a966a
guna9876 <guna05449@gmail.com>
2023-12-12 19:24:49 +0530

    first commit-in travel-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackigList />
+      <Stats />
     </div>
   );
 }
-
-export default App;
+function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do You need for Your Trip😊</h3>
+    </div>
+  );
+}
+function PackigList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <div className="stats">
+      <footer>
+        <em>💼You have X items in List,alredy packed x(x%)</em>
+      </footer>
+    </div>
+  );
+}
