0e3d125f0c8f7a81ca542db95198fd6a5c532ef0
guna9876 <guna05449@gmail.com>
2023-12-14 23:44:22 +0530

    list of items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 2, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -19,7 +25,25 @@ function Form() {
   );
 }
 function PackigList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
